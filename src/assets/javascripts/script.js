2f07e4411a7a579177d6f377d0eb0598520d778e
Muhammed Nayeem <mohammednayeem808@gmail.com>
2025-06-29 17:45:08 +0600

    Review: Completed

--- src/assets/javascripts/script.js
+++ src/assets/javascripts/script.js
@@ -14,3 +14,34 @@ navLinks.forEach((link) => {
     hamburgerBtn.classList.toggle("ri-close-large-line");
   });
 });
+
+
+//Reviews Swiper Slider Active and Implementation:
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 1,
+  speed: 400,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  grabCursor: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
